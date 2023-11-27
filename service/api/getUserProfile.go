8a5ba6715e1e3c0a439c26dd77ec90951e1ac9f9
Andrea Pizzi <pizzi.1995517@studenti.uniroma1.it>
2023-11-27 23:07:49 +0100

    backend update
    	- implemented method GET on /users/:uid endpoint
    
    Signed-off-by: Andrea Pizzi <pizzi.1995517@studenti.uniroma1.it>

--- service/api/getUserProfile.go
+++ service/api/getUserProfile.go
@@ -21,37 +21,39 @@ all information on user, in particular:
   - number of following
 */
 func (rt *_router) getUserProfile(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	follower := 0
-	following := 0
-	users := []database.User{}
-	var user database.User
-	var photos database.StreamPhotos
 
 	uid, _error := strconv.Atoi(ps.ByName("uid"))
-
 	if _error != nil {
 		w.WriteHeader(ServerError.StatusCode)
 		io.WriteString(w, ServerError.Status)
+		return
 	}
 
-	for _, user = range users {
-		if user.GetId() == uint64(uid) {
+	if user, err := rt.db.GetUser(database.Id(uid)); err != nil {
+		w.Header().Add("content-type", "text/plain")
+		w.WriteHeader(http.StatusNotFound)
+		io.WriteString(w, "user not found")
+		return
+	} else {
+		follower, _error := rt.db.GetFollower(database.Id(uid))
+		following, _error := rt.db.GetFollowing(database.Id(uid))
+		photos, _error := rt.db.GetMyStream(database.Id(uid))
 
-			// get stream of photos
-			w.Header().Add("content-type", "application/json")
-			w.WriteHeader(http.StatusOK)
-			io.WriteString(w, "get user profile corresponding to *uid*")
-			json.NewEncoder(w).Encode(database.Profile{
-				User:      user,
-				Stream:    photos,
-				Follower:  follower,
-				Following: following,
-			})
-			return
+		if _error != nil {
+			w.WriteHeader(ServerError.StatusCode)
+			io.WriteString(w, ServerError.Status)
 		}
+
+		// get stream of photos
+		w.Header().Add("content-type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, "get user profile corresponding to *uid*")
+		json.NewEncoder(w).Encode(database.Profile{
+			User:      user,
+			Stream:    photos,
+			Follower:  len(follower),
+			Following: len(following),
+		})
 	}
-	w.Header().Add("content-type", "text/plain")
-	w.WriteHeader(http.StatusNotFound)
-	io.WriteString(w, "user not found")
 
 }

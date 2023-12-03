aa5c5e65a78d0e3e19f98faf6e547a88295c4e6b
Andrea Pizzi <pizzi.1995517@studenti.uniroma1.it>
2023-12-03 13:35:08 +0100

    deleted empy file or renamed it

--- service/database/update-username.go
+++
@@ -1,7 +0,0 @@
-package database
-
-func (db *appdbimpl) SetUsername(uid Id, username string) (user User, err error) {
-
-	err = db.c.QueryRow("UPDATE Users SET username=? WHERE uid = ? RETURNING *", username, uid).Scan(&user.Uid, &user.Username)
-	return
-}

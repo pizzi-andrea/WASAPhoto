d6767c2b2f7fb0eeb46bc063183a7d22a1acf651
Andrea Pizzi <pizzi.1995517@studenti.uniroma1.it>
2023-12-04 14:49:12 +0100

    update test script
    Signed-off-by: Andrea Pizzi <pizzi.1995517@studenti.uniroma1.it>

--- demo/test_1.sh
+++ demo/test_1.sh
@@ -55,7 +55,7 @@ curl -X 'POST' \
   -d '{
   "name": "timux"
 }'
-
+sleep 5
 echo "\n=======end /session/ test=============\n"
 echo "\n=======start /users/ test=============\n"
 
@@ -79,7 +79,7 @@ curl -X 'GET' \
   'http://localhost:3000/users/?username=a' \
   -H 'accept: application/json' \
   -H 'Authorization: Bearer 1'
-
+sleep 5
 echo "\n========end test /users/================\n"
 echo "\n========start test /users/:uid================\n"
 
@@ -104,6 +104,7 @@ curl -X 'GET' \
   -H 'accept: application/json' \
   -H 'Authorization: Bearer 4'
 echo "\t\n\t\n"
+sleep 5
 echo "\n========end test /users/:uid/================\n"
 echo "\n========star test /users/:uid/followers/followerId================\n"
 
@@ -111,12 +112,12 @@ echo "\n========star test /users/:uid/followers/followerId================\n"
 curl -X 'PUT' \
   'http://localhost:3000/users/2/followers/3' \
   -H 'accept: application/json' \
-  -H 'Authorization: Bearer 1'
+  -H 'Authorization: Bearer 3'
 echo "\t\n\t\n"
 curl -X 'PUT' \
   'http://localhost:3000/users/3/followers/4' \
   -H 'accept: application/json' \
-  -H 'Authorization: Bearer 1'
+  -H 'Authorization: Bearer 4'
 echo "\t\n\t\n"
 curl -X 'PUT' \
   'http://localhost:3000/users/1/followers/2' \
@@ -127,6 +128,17 @@ curl -X 'PUT' \
   'http://localhost:3000/users/3/followers/4' \
   -H 'accept: application/json' \
   -H 'Authorization: Bearer 4'
+ 
+ echo \n dell \n 
+  
+curl -X 'DELETE' \
+  'http://localhost:3000/users/1/followers/2' \
+  -H 'accept: */*' \
+  -H 'Authorization: Bearer 2'
+  
+
+  
+
 echo "\n========end test /users/:uid/followers/followerId================\n"
 echo "\n========start test /users/:uid/followers================\n"
 curl -X 'GET' \
@@ -148,4 +160,62 @@ curl -X 'GET' \
   'http://localhost:3000/users/3/followers/' \
   -H 'accept: application/json' \
   -H 'Authorization: Bearer 4'
-echo "\n========start test /users/:uid/followers/================\n"
+sleep 5
+echo "\n========end test /users/:uid/followers/================\n"
+echo "\n========start test /users/:uid/banned/:bannedId================\n"
+
+curl -X 'PUT' \
+  'http://localhost:3000/users/2/banned/3' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer 2'\
+  -d '{
+  "username": "marioross"
+}'
+curl -X 'PUT' \
+  'http://localhost:3000/users/2/banned/5' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer 2'\
+  -d '{
+  "username": "marioross"
+}'
+
+curl -X 'PUT' \
+  'http://localhost:3000/users/3/banned/1' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer 3'\
+  -d '{
+  "username": "marioross"
+}'
+
+curl -X 'PUT' \
+  'http://localhost:3000/users/5/banned/4' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer 1'\
+  -d '{
+  "username": "marioross"
+}'
+
+echo \nput\n
+curl -X 'GET' \
+  'http://localhost:3000/users/2/banned/?limit=50' \
+  -H 'accept: application/json' \
+  -H 'Authorization: Bearer 2'
+
+echo \ndel\n
+
+curl -X 'DELETE' \
+  'http://localhost:3000/users/3/banned/1' \
+  -H 'accept: */*' \
+  -H 'Authorization: Bearer 3'
+
+
+  
+echo "\n========end test /users/::uid/banned/:bannedId================\n"
+
+sleep 5
+
+

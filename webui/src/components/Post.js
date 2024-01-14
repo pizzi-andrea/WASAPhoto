8ab0cfce2e8b6ed19067b9f9b6e955eb33a83843
Andrea Pizzi <pizzi.1995517@studenti.uniroma1.it>
2024-01-14 22:11:48 +0100

    frontend updates     - added scripts for modeling class objects: Post, Comment,       Profile

---
+++ webui/src/components/Post.js
@@ -0,0 +1,12 @@
+
+export default class Post{
+    constructor(owner, refer, location, likes, comments, timeUpdate, descriptionImg){
+        this.owner = owner
+        this.refer = refer
+        this.location = location
+        this.likes = likes
+        this.comments = comments
+        this.timeUpdate = timeUpdate
+        this.descriptionImg = descriptionImg
+    }
+}
--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -3,13 +3,18 @@ import Login from '../views/Login.vue'
 import ProfileView from '../views/ProfileView.vue'
 import ServerError from '../views/ServerError.vue'
 import ClientError from '../views/ClientError.vue'
+import { onBeforeUpdate } from 'vue'
 const router = createRouter({
 	history: createWebHashHistory(import.meta.env.BASE_URL),
 	routes: [
+		{path: '/', redirect: '/session'},
 		{path: '/error/server', component: ServerError},
 		{path: '/error/client', component: ClientError},
 		{path: '/session', component: Login},
-		{path: '/users/:id', component: ProfileView},
+		{path: '/users/:id', component: ProfileView, meta:{
+			requireAuth: true,
+		}
+	},
 		
 	]
 })

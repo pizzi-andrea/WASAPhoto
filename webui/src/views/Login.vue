f49b4b5b6789f842b601ca58abaf6d476be1e7ac
Andrea Pizzi <pizzi.1995517@studenti.uniroma1.it>
2024-01-19 22:40:39 +0100

    fixed bug in login action, now user when close web page will clear are data like perform logout action
    Signed-off-by: Andrea Pizzi <pizzi.1995517@studenti.uniroma1.it>

--- webui/src/views/Login.vue
+++ webui/src/views/Login.vue
@@ -1,126 +1,116 @@
 <script>
+import ErrorMsg from "@/components/ErrorMsg.vue";
 
 export default {
-	data: function() {
+	data() {
 		return {
 			errormsg: null,
 			loading: false,
 			username: "",
 			errorInUsername: false,
-			msgErrorData: " Errore stringa 'username' mal formattata. \n Un username valido deve avere una lunghezza compresa tra 3 e 16 caratteri di qualunque tipo.",
-			token: 0
-		}
+			msgErrorData:
+				"Errore stringa 'username' mal formattata. Un username valido deve avere una lunghezza compresa tra 3 e 16 caratteri di qualunque tipo.",
+			token: 0,
+		};
 	},
 	methods: {
-		refresh() {
-			if(localStorage.getItem('token') && localStorage.getItem('token') != '0'){
-				this.$router.replace("/users/" + localStorage.getItem('token') + "/");
-				this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
-
-			}
-			this.loading = true;
-			this.errormsg = null;
-            this.errorInUsername=false
-            this.username = ""
-			this.token = 0
+		 refresh() {
 			
+			
+				this.loading = true;
+				this.errormsg = null;
+				this.errorInUsername = false;
+				this.username = "";
+				this.token = 0;
+				localStorage.clear();
 			
 		},
 
-		async login(){
-            this.dataValidator()
-			let response = null
-			
-			try {
+		async login() {
+			this.dataValidator();
+			let response = null;
 
+			try {
 				if (this.errorInUsername) {
-					throw ("Username non valido.") 
+					throw "Username non valido.";
 				}
 				response = await this.$axios.post("/session", {
-					name: `${this.username}`
+					name: `${this.username}`,
 				});
-				
-				
-				switch(response.status) {
+
+				switch (response.status) {
 					case 200:
 					case 201:
-						this.token = response.data.Value
-						this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.token
-						localStorage.setItem('token', this.token)
-						console.log( this.$axios.get('/users/' + this.token + '/') )
-						this.$router.push('/users/' + this.token + '/')
-						
-					break
+						this.token = response.data.Value;
+						this.$axios.defaults.headers.common[
+							"Authorization"
+						] = `Bearer ${this.token}`;
+						localStorage.setItem("token", this.token);
+						console.log(this.$axios.get("/users/" + this.token + "/"));
+						this.$router.push("/users/" + this.token + "/");
+						break;
 					case 400:
-						this.$router.push('/error/client')
-						break
+						this.$router.push("/error/client");
+						break;
 					case 500:
-						this.$router.push('error/server')
-						
-
-
+						this.$router.push("error/server");
 				}
 				this.loading = false;
-
-		} catch (e) {
+			} catch (e) {
 				this.errormsg = e.toString();
-				return 
+				return;
 			}
 		},
 
-		dataValidator() { this.errorInUsername =  !( this.username.length >= 3 && this.username.length <= 16 && /^.*$/.test(this.username) )}
-
-
+		dataValidator() {
+			this.errorInUsername = !(
+				this.username.length >= 3 &&
+				this.username.length <= 16 &&
+				/^.*$/.test(this.username)
+			);
+		},
 	},
 	mounted() {
-		this.refresh()
-	}
-}
-
-
-
+		this.refresh();
+	},
+	components: {
+		ErrorMsg,
+	},
+};
 </script>
 
 <template>
-	
 	<title>WasaPhoto - login</title>
-	
-	<div class = "container form-group  mr-4 mt-4">
-			
-			
 
-				<div class="row">
-					
-					<div class="container">
-
-						<div class="row  flex-row d-inline-flex">
-							
-							<h4>Username</h4>
-							<div class="col-sm m-1 d-sm-inline-flex">
-								
-								<input v-model="username" id="usernameField" placeholder="username" minlength="3" maxlength="16"  class =" m-1 form-control" @input ="dataValidator" @focus="refresh">
-								<button class="btn btn-primary m-4" @click="login">Login/Sigin</button>
-							</div>
-							
-						</div>
-						
-						
+	<div class="container form-group  mr-4 mt-4">
+		<div class="row">
+			<div class="container">
+				<div class="row  flex-row d-inline-flex">
+					<h4>Username</h4>
+					<div class="col-sm m-1 d-sm-inline-flex">
+						<input
+							v-model="username"
+							id="usernameField"
+							placeholder="username"
+							minlength="3"
+							maxlength="16"
+							class="m-1 form-control"
+							@input="dataValidator"
+							@focus="refresh"
+						/>
+						<button class="btn btn-primary m-4" @click="login">Login/Sigin</button>
 					</div>
-
 				</div>
-				
-		
+			</div>
+		</div>
 	</div>
 
-		
-		<hr>
+	<hr>
 
-		<ErrorMsg v-if="errormsg" :msg="errormsg"></ErrorMsg>
-		<br>
-		<ErrorMsg v-if="errorInUsername" :msg="msgErrorData"></ErrorMsg>		
-	
+	<ErrorMsg v-if="errormsg" :msg="errormsg"></ErrorMsg>
+	<br>
+	<ErrorMsg v-if="errorInUsername" :msg="msgErrorData"></ErrorMsg>
 </template>
 
 <style>
 </style>
-

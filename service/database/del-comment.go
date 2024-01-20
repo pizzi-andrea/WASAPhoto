ef3abc2f7b67e11eacb76e8aa151fefd38770d17
Andrea Pizzi <pizzi.1995517@studenti.uniroma1.it>
2024-01-20 23:49:58 +0100

    backend updates 	- fixed bad implementation GetComment method (db and go code) 	- fixed bad use of identificator comment in uncomment method
    Signed-off-by: Andrea Pizzi <pizzi.1995517@studenti.uniroma1.it>

--- service/database/del-comment.go
+++ service/database/del-comment.go
@@ -3,12 +3,14 @@ package database
 import (
 	"database/sql"
 	"errors"
+	"fmt"
 )
 
 // DelBan allow to remove ban on user to. When banned is removed user banned will research and watch all information about from user.
 // if ban deleted with success function return true and nil error for error if ban not exist function return false and nil value
 // if occured error function return false and not-nil error value.
 func (db *appdbimpl) DelComment(commentId Id) (r bool, err error) {
+	fmt.Println(commentId)
 	_, err = db.c.Exec("DELETE FROM Comments WHERE commentId = ?", commentId)
 
 	if err == nil {
--- service/database/get-comment.go
+++ service/database/get-comment.go
@@ -9,7 +9,7 @@ import (
 func (db *appdbimpl) GetComment(commentId Id) (comment *Comment, err error) {
 	var c Comment
 
-	err = db.c.QueryRow("SELECT * FROM Comments WHERE commentId").Scan(&c.CommentId, &c.Author, &commentId, &c.Text, &c.TimeStamp)
+	err = db.c.QueryRow("SELECT * FROM Comments WHERE commentId = ?", commentId).Scan(&c.CommentId, &c.Author.Uid, &commentId, &c.Text, &c.TimeStamp)
 
 	if errors.Is(err, sql.ErrNoRows) {
 		err = nil
